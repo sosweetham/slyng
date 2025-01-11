26653a92959e776a8b9f342f1fb1995343461173
SoSweetHam <sosweetham@gmail.com>
2025-01-11 21:12:19 +0530

    feat: setup base

---
+++ apps/nexus/main.ts
@@ -0,0 +1,9 @@
+import { type Context, Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c: Context) => {
+    return c.text("Hello Hono!");
+});
+
+Deno.serve(app.fetch);
---
+++ apps/slyng/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {page} from '$app/state';
+	let { children } = $props();
+
+    // get page path
+    let pagePath = page.route
+</script>
+
+{JSON.stringify(pagePath)}
+
+{@render children?.()}
